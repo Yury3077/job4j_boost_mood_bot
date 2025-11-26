acf40072f06b8c173d85222f42472ada7a5d63b9
Yury Balykov <juser89@mail.ru>
2025-11-26 10:09:53 +0300

    Add BeanNameAware

---
+++ src/main/java/ru/job4j/services/BeanNameAware.java
@@ -0,0 +1,24 @@
+package ru.job4j.services;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeanNameAware implements ApplicationContextAware {
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+        System.out.println("ApplicationContext set in ApplicationContextAwareExample");
+    }
+
+    public void displayAllBeanNames() {
+        String[] beanNames = applicationContext.getBeanDefinitionNames();
+        System.out.println("Beans in ApplicationContext:");
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
+    }
+}
